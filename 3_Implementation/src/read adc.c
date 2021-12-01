ea0bb8f3a6d463ed35bd76d6446589624062da60
pksgithb <94304459+pksgithb@users.noreply.github.com>
2021-12-01 23:53:30 +0530

    Update read adc.c

--- 3_Implementation/src/read adc.c
+++ 3_Implementation/src/read adc.c
@@ -1,24 +1,12 @@
-/***********************************************
- * Name:
- *    ReadADC
- * In:
- *    uint8_t channel
- * Out:
- *    adc_value
- * Description:
- *    Function to Read ADC Value
- *	  The channel to be read is entered as input
- *
- ************************************************/
 uint16_t ReadADC (uint8_t channel)
 {
 	uint16_t adc_value;
 
-	ADMUX &= 0xF0;                    			//Clear the older channel that was read
-	ADMUX = channel | (1<<REFS0);				// ADC input channel set to channel and Vref <- AVcc
-	ADCSRA |= (1<<ADSC);						// Start conversion
-	while (ADCSRA & (1<<ADSC));					// wait for conversion to complete
+	ADMUX &= 0xF0;                    			
+	ADMUX = channel | (1<<REFS0);			
+	ADCSRA |= (1<<ADSC);						
+	while (ADCSRA & (1<<ADSC));					
 	
-	adc_value = ADCW;							//Store ADC value
+	adc_value = ADCW;							
 	return adc_value;
-}//~~ end of ReadADC
+}
