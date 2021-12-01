096d25e57f858c46df32c7819392a2f041e0315f
pksgithb <94304459+pksgithb@users.noreply.github.com>
2021-12-01 18:43:46 +0530

    Create function.h

---
+++ 3_Implementation/inc/function.h
@@ -0,0 +1,17 @@
+## Header files
+
+/**
+ * @file functions.h
+ * @author Y.Lakshmi Pavan Kalyan Reddy
+ * @brief header files declerations for executing the program code
+ * @version 0.1
+ * @date 2021-11-24
+ */
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "util/delay.h"
+#include <stdbool.h>
+
+/** @brief the header files used in source code */
