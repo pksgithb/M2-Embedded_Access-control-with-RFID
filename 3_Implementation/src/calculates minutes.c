e546aefc0ac5dc30dbcbd6ec0452b8feb560c21d
pksgithb <94304459+pksgithb@users.noreply.github.com>
2021-12-01 23:48:59 +0530

    Update calculates minutes.c

--- 3_Implementation/src/calculates minutes.c
+++ 3_Implementation/src/calculates minutes.c
@@ -42,34 +42,4 @@ void CalculateMinutes (bool time_display)
 		break;
 	}	
 }
-void CalculateHours (bool time_display)
-{
-	switch (time_display){
-		case 0:
-				if (hour/10 == 0){
-					WriteDisplays(0,3);
-					WriteDisplays(hour,2);
-				}
-	
-				else{
-					WriteDisplays(1,3);	
-					WriteDisplays(hour%10,2);	
-				}
-		break;
-		
-		case 1:
-				if (hour/10 == 0){
-					WriteDisplays(0,3);
-					_delay_ms(50);
-					WriteDisplays(hour,2);
-					_delay_ms(50);
-				}
-				
-				else{
-					WriteDisplays(1,3);
-					_delay_ms(50);
-					WriteDisplays(hour%10,2);
-					_delay_ms(50);
-				}
-				break;	
-	}
+
