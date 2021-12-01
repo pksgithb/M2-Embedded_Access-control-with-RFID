8ce675b3f82aa222847b7f1d69a4e7fb011e6210
pksgithb <94304459+pksgithb@users.noreply.github.com>
2021-12-01 18:51:02 +0530

    Create calculates seconds.c

---
+++ 3_Implementation/src/calculates seconds.c
@@ -0,0 +1,56 @@
+/***********************************************
+ * Name:
+ *    CalculateSeconds
+ * In:
+ *    --
+ * Out:
+ *    --
+ * Description:
+ *    Function to write on 7 segments display
+ *    It receives two arguments: the digit  
+ *    and the display number to to be writed 
+ *
+ ************************************************/
+void CalculateSeconds (bool time_display)
+{
+	switch (time_display){
+		case 0:
+				if (seconds/10 == 0 && seconds < 10){
+					WriteDisplays(0,1);
+					WriteDisplays(seconds,0);
+				}
+
+				else if (seconds%10 != 0){
+					WriteDisplays(seconds/10,1);
+					WriteDisplays(seconds%10,0);
+				}
+	
+				else if (seconds%10 == 0){
+					WriteDisplays(seconds/10,1);
+					WriteDisplays(0,0);
+				}
+		break;
+		
+		case 1:
+				if (seconds/10 == 0 && seconds < 10){
+					WriteDisplays(0,1);
+					_delay_ms(25);
+					WriteDisplays(seconds,0);
+					_delay_ms(25);
+				}
+
+				else if (seconds%10 != 0){
+					WriteDisplays(seconds/10,1);
+					_delay_ms(25);
+					WriteDisplays(seconds%10,0);
+					_delay_ms(25);
+				}
+				
+				else if (seconds%10 == 0){
+					WriteDisplays(seconds/10,1);
+					_delay_ms(25);
+					WriteDisplays(0,0);
+					_delay_ms(25);
+				}
+		break;
+	}
